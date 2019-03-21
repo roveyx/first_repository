7011119f89ed1714d6c740e83644265192de6755
guoyongxiao <805372666@qq.com>
2019-03-21 14:27:01 +0800

    Update js error

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length ; i++){
-	acc[i].onclick=fonction(){
-		this.calssList.toggle("active")
-		var panel = this.netElementsSibling;
-		if(paner.style.display == 'block'){
-			paner.style.display = "none"
+	acc[i].onclick=function(){
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if(panel.style.display === "block"){
+			panel.style.display = "none";
 		}else{
-			paner.style.display = 'block'
+			panel.style.display = "block";
 		}
 	}
 }
